ee90100017ca74a6109788f279446dc2f31157dd
christides11 <29593332+christides11@users.noreply.github.com>
2022-04-27 17:23:56 -0700

    Selectable navigation.

--- Assets/_Project/Scripts/UI/UITweening/Selectable.cs
+++ Assets/_Project/Scripts/UI/UITweening/Selectable.cs
@@ -2,16 +2,22 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace rwby.ui
 {
-    public class Selectable : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler, ISubmitHandler, IPointerClickHandler
+    public class Selectable : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler, ISubmitHandler, IPointerClickHandler, IMoveHandler
     {
         public bool active = true;
         public UnityEvent onSelect;
         public UnityEvent onDeselect;
         public UnityEvent onSubmit;
 
+        public Selectable selectOnUp;
+        public Selectable selectOnDown;
+        public Selectable selectOnLeft;
+        public Selectable selectOnRight;
+        
         public virtual void Awake()
         {
             
@@ -59,6 +65,34 @@ namespace rwby.ui
         public virtual void OnPointerClick(PointerEventData eventData)
         {
             onSubmit.Invoke();
+        }
+        
+        public virtual void OnMove(AxisEventData eventData)
+        {
+            switch (eventData.moveDir)
+            {
+                case MoveDirection.Right:
+                    Navigate(eventData, selectOnRight);
+                    break;
+
+                case MoveDirection.Up:
+                    Navigate(eventData, selectOnUp);
+                    break;
+
+                case MoveDirection.Left:
+                    Navigate(eventData, selectOnLeft);
+                    break;
+
+                case MoveDirection.Down:
+                    Navigate(eventData, selectOnDown);
+                    break;
+            }
+        }
+        
+        public virtual void Navigate(AxisEventData eventData, Selectable sel)
+        {
+            if (sel != null && sel.active)
+                eventData.selectedObject = sel.gameObject;
         }
     }
 }
