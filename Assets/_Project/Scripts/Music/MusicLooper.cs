bb4877f1e95a3e705fe91d296dac2c4c4c4b95ee
Chris Collins <29593332+christides11@users.noreply.github.com>
2022-08-22 21:12:26 -0700

    Basic music looper

---
+++ Assets/_Project/Scripts/Music/MusicLooper.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace rwby
+{
+    public class MusicLooper : MonoBehaviour
+    {
+        public AudioSource[] audioSources;
+        public AudioClip song;
+
+        public double introBoundary;
+        public double loopPointBoundary;
+        
+        private int nextSource = 0;
+        private double nextEventTime;
+
+        private void Start()
+        {
+            StartSong();
+        }
+
+        public void StartSong()
+        {
+            audioSources[nextSource].clip = song;
+            audioSources[nextSource].PlayScheduled(AudioSettings.dspTime);
+            audioSources[nextSource].SetScheduledEndTime(AudioSettings.dspTime + introBoundary);
+            nextSource = GetNextSource(nextSource);
+            
+            audioSources[nextSource].clip = song;
+            audioSources[nextSource].PlayScheduled(AudioSettings.dspTime + introBoundary);
+            audioSources[nextSource].time = (float)introBoundary;
+            audioSources[nextSource].SetScheduledStartTime(AudioSettings.dspTime + introBoundary);
+            audioSources[nextSource].SetScheduledEndTime(AudioSettings.dspTime + loopPointBoundary);
+
+            nextEventTime = AudioSettings.dspTime + loopPointBoundary;
+        }
+
+        private void Update()
+        {
+            double time = AudioSettings.dspTime;
+            if (time + 1.0f > nextEventTime)
+            {
+                int flip = GetNextSource(nextSource);
+
+                audioSources[flip].time = (float)introBoundary;
+                audioSources[flip].PlayScheduled(nextEventTime);
+                
+                nextEventTime = nextEventTime + (loopPointBoundary - introBoundary);
+
+                nextSource = flip;
+            }
+        }
+
+        public int GetNextSource(int val)
+        {
+            return val == 0 ? 1 : 0;
+        }
+    }
+}
